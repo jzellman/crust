f30e1369064280ef886b7f14e2650d51fb70a041
Jeff Zellman <jzellman@gmail.com>
2017-05-19 15:19:44 -0500

    chore: bump version to 0.0.3.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'web.py crust - extensions for your web.py app',
+    'description': 'web.py-crust - Various extensions and niceties for web.py',
     'author': 'Jeff Zellman',
     'url': 'http://github.com/jzellman/webpy-crust',
     'download_url': 'http://github.com/jzellman/webpy-crust',
     'author_email': 'jzellman@gmail.com',
-    'version': '0.0.2',
+    'version': '0.0.3',
     'install_requires': ['web.py', 'nose'],
     'packages': ['crust'],
     'scripts': [],
