56ee72c72c543af746839ddcdef646576d0e3f6c
Jeff Zellman <jzellman@gmail.com>
2017-06-30 13:23:21 -0500

    feat: expose sslify in top level.

--- crust/__init__.py
+++ crust/__init__.py
@@ -2,3 +2,4 @@ from .crypt import SimpleCryptor, Bcryptor
 from .mailer import debugmail, sendmail
 from .session import Session, Flash, RedisStore
 from .render import RenderPartial
+from .sslify import sslify
