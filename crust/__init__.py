6405f58ee34d9b8ccd305b50e96f652d4210eb30
Jeff Zellman <jzellman@gmail.com>
2017-06-30 13:23:42 -0500

    feat: expose AuthorizationProcessor and Protector in top level.

--- crust/__init__.py
+++ crust/__init__.py
@@ -3,3 +3,4 @@ from .mailer import debugmail, sendmail
 from .session import Session, Flash, RedisStore
 from .render import RenderPartial
 from .sslify import sslify
+from .auth import AuthorizationProcessor, Protector
